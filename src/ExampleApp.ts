7fedc8f26d8c99b6f09a18957d027e12c60b4da2
Daniel Keefe <dfk@umn.edu>
2024-10-08 17:02:09 -0500

    in class work

--- src/ExampleApp.ts
+++ src/ExampleApp.ts
@@ -74,12 +74,57 @@ export class ExampleApp extends gfx.GfxApp
     {
         const mesh: gfx.Mesh3 = new gfx.Mesh3();
         const vertices: gfx.Vector3[] = [];
+        const normals: gfx.Vector3[] = [];
         const indices: number[] = [];
 
         // TODO: Fill in the vertices and indices arrays
 
+        const numSegments = 25;
+        const angleInc = -2.0 * Math.PI / numSegments;
+        const rad = 0.5;
+        const height = 2;
+
+        // first create the vertices
+        for (let i=0; i<=numSegments; i++) {
+            const x = rad * Math.cos(i*angleInc);
+            const z = rad * Math.sin(i*angleInc);
+            const n = gfx.Vector3.normalize(new gfx.Vector3(x, 0, z));
+            
+            vertices.push(new gfx.Vector3(x,height/2,z));
+            normals.push(n);
+
+            vertices.push(new gfx.Vector3(x,-height/2,z));
+            normals.push(n);
+        }
+
+        // now create the indices
+        for (let i=0; i < numSegments; i++) {
+            indices.push(2*i);    // 0, 2, 4
+            indices.push(2*i+1);  // 1, 3, 5
+            indices.push(2*i+2);  // 2, 4, 6
+
+            indices.push(2*i+1);      // 1, 3
+            indices.push(2*(i+1)+1);  // 3, 5
+            indices.push(2*(i+1));    // 2, 4
+        }
+
+        /**
+        vertices.push(new gfx.Vector3(0,1,0));
+        vertices.push(new gfx.Vector3(0,0,0));
+        vertices.push(new gfx.Vector3(1,1,0));
+        vertices.push(new gfx.Vector3(1,0,0));
+
+        indices.push(0);
+        indices.push(1);
+        indices.push(2);
+
+        indices.push(1);
+        indices.push(3);
+        indices.push(2);
+        **/
 
         mesh.setVertices(vertices);
+        mesh.setNormals(normals);
         mesh.setIndices(indices);
         return mesh;
     }
